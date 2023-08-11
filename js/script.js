5fa12902f58a92dbe7f57cd5f66bf98b5ce0f8f8
srakatae <114945530+srakatae@users.noreply.github.com>
2023-08-11 17:11:08 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+        $('.header__burger,.header__menu, ').toggleClass('active');
+        
+$('header__link').click('active');
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
